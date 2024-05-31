11d74362974287e24c5af68ac3635e69b9f06666
Mehul Goel <mehulgoel873@gmail.com>
2024-05-31 00:00:45 -0700

    nearly final model

--- nanogpt/v2.py
+++ nanogpt/v2.py
@@ -13,14 +13,20 @@ from torch.nn import functional as F
 import time
 
 #hyperparameters
-batch_size = 32
-block_size = 8
+batch_size = 64
+block_size = 256
 max_iters = 5000
 eval_interval = 500
-learning_rate = 1e-3
+learning_rate = 3e-4
 device =  'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32
+
+n_embd = 384
+n_head = 6
+n_layer = 6
+
+dropout = 0.2
+
 #-------------
 
 print(device)
@@ -76,6 +82,9 @@ class Head(nn.Module):
         self.query = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+
+        self.dropout = nn.Dropout(dropout)
     def forward(self, x):
       B, T, C = x.shape
       k = self.key(x)
@@ -84,6 +93,7 @@ class Head(nn.Module):
       wei = q @ k.transpose(-2, -1) * ((C**-0.5))
       wei = wei.masked_fill(self.tril[:T, :T] == 0 , float('-inf'))
       wei = F.softmax(wei, dim=-1)
+      wei = self.dropout(wei)
       v = self.value(x)
       out = wei @ v 
       return out
@@ -93,10 +103,11 @@ class MultiHeadAttention(nn.Module):
     super().__init__()
     self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
     self.proj = nn.Linear(n_embd, n_embd)
+    self.dropout = nn.Dropout(dropout)
   
   def forward(self, x):
     out = torch.cat([h(x) for h in self.heads], dim=-1)
-    out = self.proj(out)
+    out = self.dropout(self.proj(out))
     return out
   
 
@@ -107,6 +118,7 @@ class FeedForward(nn.Module):
             nn.Linear(n_embd, 4 * n_embd),
             nn.ReLU(),
             nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
         )
     def forward(self, x):
       return self.net(x)
@@ -117,10 +129,12 @@ class Block(nn.Module):
         head_size = n_embd // n_head
         self.sa = MultiHeadAttention(n_head, head_size)
         self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
 
     def forward(self, x):
-       x = x + self.sa(x)
-       x = x + self.ffwd(x)
+       x = x + self.sa(self.ln1(x))
+       x = x + self.ffwd(self.ln2(x))
        return x
 
 
@@ -130,11 +144,8 @@ class BigramLanguageModel(nn.Module):
     super().__init__()
     self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
     self.position_embedding_table = nn.Embedding(block_size, n_embd)
-    self.blocks = nn.Sequential(
-       Block(n_embd, n_head=4),
-       Block(n_embd, n_head=4),
-       Block(n_embd, n_head=4),
-    )
+    self.blocks = nn.Sequential(*[Block(n_embd,n_head=n_head) for _ in range(n_layer)]) 
+    self.ln_f = nn.LayerNorm(n_embd)
     self.lm_head = nn.Linear(n_embd, vocab_size)
 
   def forward(self, idx, targets=None):
@@ -144,6 +155,7 @@ class BigramLanguageModel(nn.Module):
     pos_embd = self.position_embedding_table(torch.arange(T, device=device))
     x = tok_embd + pos_embd
     x = self.blocks(x)
+    x = self.ln_f(x)
     logits = self.lm_head(x)
 
 
@@ -188,5 +200,5 @@ for iter in range(max_iters):
   optimizer.step()
 
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
-print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
+print(decode(m.generate(context, max_new_tokens=2000)[0].tolist()))
 print("\n\n Total Execution Time: ", (time.time() - startTime))
