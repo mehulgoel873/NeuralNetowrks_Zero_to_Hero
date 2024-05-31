936c5bbe94e3d42771ff436178b7c17449b81c5e
Mehul Goel <mehulgoel873@gmail.com>
2024-05-30 23:46:53 -0700

    modified

--- nanogpt/v2.py
+++ nanogpt/v2.py
@@ -18,7 +18,7 @@ block_size = 8
 max_iters = 5000
 eval_interval = 500
 learning_rate = 1e-3
-device =  'cpu'
+device =  'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 32
 #-------------
@@ -92,49 +92,16 @@ class MultiHeadAttention(nn.Module):
   def __init__(self, num_heads, head_size):
     super().__init__()
     self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
-    self.proj = nn.Linear(n_embd, n_embd)
   
   def forward(self, x):
-    out = torch.cat([h(x) for h in self.heads], dim=-1)
-    out = self.proj(out)
-    return out
-  
-
-class FeedForward(nn.Module):
-    def __init__(self, n_embd):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(n_embd, 4 * n_embd),
-            nn.ReLU(),
-            nn.Linear(4 * n_embd, n_embd),
-        )
-    def forward(self, x):
-      return self.net(x)
-    
-class Block(nn.Module):
-    def __init__(self, n_embd, n_head):
-        super().__init__()
-        head_size = n_embd // n_head
-        self.sa = MultiHeadAttention(n_head, head_size)
-        self.ffwd = FeedForward(n_embd)
-
-    def forward(self, x):
-       x = x + self.sa(x)
-       x = x + self.ffwd(x)
-       return x
-
-
+    return torch.cat([h(x) for h in self.heads], dim=-1)
 
 class BigramLanguageModel(nn.Module):
   def __init__(self):
     super().__init__()
     self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
     self.position_embedding_table = nn.Embedding(block_size, n_embd)
-    self.blocks = nn.Sequential(
-       Block(n_embd, n_head=4),
-       Block(n_embd, n_head=4),
-       Block(n_embd, n_head=4),
-    )
+    self.sa_heads = MultiHeadAttention(4, n_embd//4)
     self.lm_head = nn.Linear(n_embd, vocab_size)
 
   def forward(self, idx, targets=None):
@@ -143,7 +110,7 @@ class BigramLanguageModel(nn.Module):
     tok_embd = self.token_embedding_table(idx)
     pos_embd = self.position_embedding_table(torch.arange(T, device=device))
     x = tok_embd + pos_embd
-    x = self.blocks(x)
+    x = self.sa_heads(x)
     logits = self.lm_head(x)
 
 
